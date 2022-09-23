93169be6e895538dadef6a625076f0617f790390
Lucas Gabriel Guilherme dos Santos <lucasggs.com@gmail.com>
2022-09-23 14:02:56 -0300

    feat: inserindo javadoc

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Projeto de pesquisa em Qualidade de Software 
 
+
+Para gerar a documentação com JavaDoc execute o seguintes comandos:
+
+```
+mvn javadoc:javadoc
+```
+
 Para executa este projeto execute os seguintes comandos:
 
 ## Para limpar o cache da execução anterior
@@ -13,18 +20,26 @@ mvn clean compile
 mvn evosuite:clean
 ```
 
-## Para gerar novos testes e exportá-los para a localização correta
+## Utilizando o EvoSuite com o Maven
+
+Para gerar novos testes de forma automática com o EvoSuite e em seguida exportá-los para a localização correta no projeto execute o seguinte comando:
+
 ```
 mvn evosuite:generate evosuite:export 
 ```
 
 ## Para executar os testes
 
+Para executar os testes (feitos manualmente ou automaticamente) execute o seguinte comando:
+
+
 ```
 mvn test
 ```
 
-## Para coletar a coverage dos testes 
+## Para coletar a cobertura dos testes 
+
+Para quem estiver empregando o EvoSuite como ferramenta de suporte na escrita dos teste é necessário empregar 
 
 ```
 mvn org.pittest:pittest-maven:mutationCoverage
--- src/main/java/example/Controllers/AgenciaController.java
+++ src/main/java/example/Controllers/AgenciaController.java
@@ -6,57 +6,112 @@ import example.Models.Agencia;
 import example.Models.Cliente;
 import example.Models.Destinos;
 
+/**
+ * @author Lucas Gabriel
+ */
 public class AgenciaController {
     
     public Agencia agencia;
     
+    /**
+     * 
+     * @param agencia
+     */
     public AgenciaController(Agencia agencia) {
         this.agencia = agencia;
     }
 
+    /**
+     * 
+     * @return
+     */
     public Agencia buscarAgencia() {
         return this.agencia;
     }
 
+    /**
+     * 
+     * @param agencia
+     */
     public void criarAgencia(Agencia agencia) {
         this.agencia = agencia;
     }
 
+    /**
+     * 
+     * @param agencia
+     */
     public void atualizarAgencia(Agencia agencia) {
         this.agencia = agencia;
     }
 
+    /**
+     * 
+     */
     public void deletarAgencia() {
         this.agencia = null;
     }
 
+    /**
+     * 
+     * @return
+     */
     public ArrayList<Cliente> buscarClientes() {
         return this.agencia.getClientes();
     }
     
+    /**
+     * 
+     * @param cliente
+     */
     public void criarClientes(Cliente cliente) {
         this.agencia.adicionaCliente(cliente);
     }
     
+    /**
+     * 
+     * @param index
+     * @param cliente
+     */
     public void atualizarClientes(int index, Cliente cliente) {
         this.agencia.adicionaCliente(cliente);
     }
     
+    /**
+     * 
+     * @param index
+     */
     public void deletarClientes(int index) {
         this.agencia.getClientes().remove(index);
     }
 
+    /**
+     * 
+     * @param cliente
+     */
     public void buscarDestinos(int cliente) {
         this.agencia.getClientes().get(cliente).getRoteiro();
     }
+
+    /**
+     * 
+     * @param escolha
+     * @param desconto
+     */
     public void criarDestinos(Destinos escolha, double desconto) {
         this.agencia.adicionaEscolha(escolha, desconto);
     }
 
+    /**
+     * 
+     */
     public void atualizarDestinos() {
         return;
     }
 
+    /**
+     * 
+     */
     public void deletarDestinos() {
         return;
     }
